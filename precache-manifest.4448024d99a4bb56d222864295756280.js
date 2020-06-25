076110f5673d6421973743f5700fa66c895b899b
Andrzej <and.przytula@gmail.com>
2020-06-25 12:10:31 -0500

    Updates

---
+++ precache-manifest.4448024d99a4bb56d222864295756280.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1fd56db059297c23f18edbe676a858",
+    "url": "/keepYourNotes/index.html"
+  },
+  {
+    "revision": "74b38238dd6ed3226346",
+    "url": "/keepYourNotes/static/js/2.d53c014f.chunk.js"
+  },
+  {
+    "revision": "f2cf06de29a20da610aa",
+    "url": "/keepYourNotes/static/js/main.2642c08b.chunk.js"
+  },
+  {
+    "revision": "2996483c89461ae9b27c",
+    "url": "/keepYourNotes/static/js/runtime-main.3d1794dd.js"
+  }
+]);
